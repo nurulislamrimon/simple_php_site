b7e13ac2d13088b0cdf57e622c508602babf0385
nurulislamrimon <nurulislamrimon@gmail.com>
2025-08-04 19:56:20 +0600

    Request Uri get process implimented

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,3 +1,8 @@
+<?php
+
+$uri = $_SERVER["REQUEST_URI"];
+echo $uri;
+?>
 <nav class="bg-gray-800">
   <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div class="relative flex h-16 items-center justify-between">
